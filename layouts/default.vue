ef97463986c2effd645972df769bf1ccc65b4109
logan laski <laskilogan@gmail.com>
2022-09-06 21:41:37 -0500

    [initial upload]

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h2>Hello world</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam perspiciatis exercitationem possimus provident, minima sit esse similique aliquam officia illo?
+        </p>
+    </div>
+</template>
